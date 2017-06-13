ad0231bbcf27fd1f12afc851d11278e64741d4e9
ACE User <aceuser@e140-s17.lab.lagcc>
2017-06-12 21:16:58 -0400

    Added toggle to nav

--- js/script.js
+++ js/script.js
@@ -1,1 +1,15 @@
-console.log('hola')
+var topNavList = document.querySelector('#top-nav-list');
+var topNavToggle = document.querySelector('#top-nav-toggle');
+var navCollapsed = true;
+
+topNavToggle.addEventListener('click', function(){
+	if(navCollapsed === true) {
+		topNavList.classList.remove('nav-list-collapse');
+		topNavToggle.innerHTML = '<span class="fa fa-times" aria-hidden="true"></span>';
+		navCollapsed = false;
+	} else {
+		topNavList.classList.add('nav-list-collapse');
+		navCollapsed = true;
+		topNavToggle.innerHTML = '<span class="fa fa-bars" aria-hidden="true"></span>';
+	}
+});
